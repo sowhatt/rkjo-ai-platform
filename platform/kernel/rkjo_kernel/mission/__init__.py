74aad5a8348da4c2de77021cf6a7627f86e3f1b5
sowhatt <34173130+sowhatt@users.noreply.github.com>
2026-09-15 00:22:00 +0200

    feat(mission): add mission package exports

---
+++ platform/kernel/rkjo_kernel/mission/__init__.py
@@ -0,0 +1,10 @@
+"""Mission primitives for RKJO executions."""
+
+from rkjo_kernel.mission.execution_context import ExecutionContext
+from rkjo_kernel.mission.models import Mission, MissionStatus
+
+__all__ = [
+    "ExecutionContext",
+    "Mission",
+    "MissionStatus",
+]
